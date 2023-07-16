f0ac10b0b7ac60bc0f7c4c4063c095bc9be32327
luccaz <luccazoppi@gmail.com>
2023-07-15 23:16:09 -0300

    added plot of return history

--- train_agent.py
+++ train_agent.py
@@ -1,7 +1,4 @@
 import os
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 from gymnasium.wrappers.time_limit import TimeLimit
@@ -9,49 +6,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dqn_agent import DQNAgent
 from utils import reward_engineering, BOARD_SZ, TIME_LIMIT, MAP_NAME, FIXED_SEED
-import tensorflow as tf
 
 
 NUM_EPISODES = 300  # Number of episodes used for training
-tf.compat.v1.disable_eager_execution()
 
+# generates map from the seed
 map_desc = generate_random_map(size=BOARD_SZ, seed = FIXED_SEED)
 env = gym.make('FrozenLake-v1', desc=map_desc, map_name=MAP_NAME, is_slippery=True)
-env = TimeLimit(env, TIME_LIMIT)
+env = TimeLimit(env, TIME_LIMIT) # generates truncated state upon reaching time limit
 action_size = env.action_space.n
 
 # Creating the DQN agent
 agent = DQNAgent(action_size)
 
-# Checking if weights from previous learning session exists
+# Try using previously obtained weights
 if os.path.exists('frozen_lake.h5'):
     print('Loading weights from previous learning session.')
     agent.load("frozen_lake.h5")
 else:
     print('No weights found from previous learning session.')
-done = False
+
 batch_size = BOARD_SZ  # batch size used for the experience replay
 return_history = []
 
 for episode in range(1, NUM_EPISODES + 1):
-    #print(episodes)
     # Reset the environment
     state, _ = env.reset()
-    state = np.array([state])
+    state = np.array([state]) # for compatibility with Keras
     # Cumulative reward is the return since the beginning of the episode
     cumulative_reward = 0.0
-    prev_action = -1
+    # previous time step action
+    prev_action = -1 
     for time in range(1, TIME_LIMIT+1):
         # Select action
         action = agent.act(state)
         # Take action, observe reward and new state
         next_state, completed, terminated, truncated, _ = env.step(action)
         next_state = np.array([next_state])
-        # Making reward engineering to allow faster training
+        # Modifying reward
         reward = reward_engineering(state, prev_action, action, completed, next_state, terminated, truncated)
         prev_action = action
         # Appending this experience to the experience replay buffer
-        agent.append_experience(state, action, reward, next_state, completed)
+        agent.append_experience(state, action, reward, next_state, terminated)
         state = next_state
         # Accumulate reward
         cumulative_reward = agent.gamma * cumulative_reward + reward
@@ -62,15 +58,14 @@ for episode in range(1, NUM_EPISODES + 1):
         # We only update the policy if we already have enough experience in memory
         if len(agent.replay_buffer) > 2 * batch_size:
             loss = agent.replay(batch_size)
+            print("loss: {:.3f}".format(loss))
     return_history.append(cumulative_reward)
     agent.update_epsilon()
     if episode % 20:
         #Saving the model to disk
         agent.save("frozen_lake.h5")
-#         plt.plot(return_history, 'b')
-#         plt.xlabel('Episode')
-#         plt.ylabel('Return')
-#         plt.show(block=False)
-#         plt.pause(0.1)
-#         plt.savefig('dqn_training.eps', format='eps')
-# plt.pause(1.0)
+
+plt.plot(return_history, 'b')
+plt.xlabel('Episode')
+plt.ylabel('Return')
+plt.savefig('dqn_training.eps', format='eps')
