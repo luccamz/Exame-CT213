4e21d44ac0e4683c3bc2a6b5957aa0e0367d50f1
luccaz <luccazoppi@gmail.com>
2023-07-16 00:28:03 -0300

    improved comments and organized params

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+from numpy import dot
+
+# gym enviroment params
 BOARD_SZ = 4
 MAP_NAME = "{}x{}".format(BOARD_SZ, BOARD_SZ)
-ACTION_N = 4
 TIME_LIMIT = 10*BOARD_SZ
 FIXED_SEED = 2
 
+# maps the flattened state displacement to the corresponding action
 displacement_to_action = { 
      0 : -1,
     -BOARD_SZ : 3,
@@ -12,30 +15,45 @@ displacement_to_action = {
      BOARD_SZ : 1 
 }
 
-def reward_engineering(state, prev_action, action, completed, next_state, terminated, time_over):
+# rewards and punishments coefficients
+rp = {
+    "completed" : 100.,
+    "goal_direction" : 10.,
+    "deliberate_action" : 5.,
+    "lost_on_time" : -80.,
+    "stuck" : -20.,
+    "moving_backwards" : -20.,
+    "fell_in_hole" : -200.,
+}
+
+def reward_engineering(state: int, prev_action: int, action: int, completed: int, next_state: int, terminated: bool, lost_on_time: bool) -> float:
     """
-    Makes reward engineering to better guided training in the Frozen Lake environment.
+    Modifies rewards for better guided training in the Frozen Lake environment.
 
     :param state: state.
     :type state: NumPy array with dimension (1, 1).
+    :param prev_action: previous time step action.
+    :type prev_action: int.
     :param action: action.
     :type action: int.
-    :param reward: original reward.
-    :type reward: float.
+    :param completed: whether objective was reached
+    :type completed: float.
     :param next_state: next state.
     :type next_state: NumPy array with dimension (1, 1).
+    :param terminated: whether the episode ended with the action
+    :type terminated: float.
+    :param completed: whether objective was reached
+    :type completed: float.
     :return: modified reward.
     :rtype: float.
     """
-    a = 10.
-    b = 2.
-    d = next_state[0] - state[0]
-    action_taken = displacement_to_action[d]
-    #print(d)
-    #print(action_taken)
-    correct_direc = 1 if action_taken == 2 or action_taken == 1 else -1
-    got_stuck = (d == 0)
-    fell_in_a_hole = completed != 1 and terminated and not time_over
-    regressing = (prev_action - action) % 2 
-    return 100*completed-100*fell_in_a_hole-20*(got_stuck+regressing)-80*time_over+a*correct_direc+b*(action_taken == action)
-
+    displacement = next_state[0] - state[0]
+    action_taken = displacement_to_action[displacement]
+    deliberate_action = int(action_taken == action)
+    goal_direction = 1 if action_taken == 2 or action_taken == 1 else -1
+    stuck = int(displacement == 0)
+    fell_in_hole = int(completed != 1 and terminated and not lost_on_time)
+    moving_backwards = (prev_action - action) % 2
+    loc = locals()
+    states = list(map(loc.get, rp.keys()))
+    return dot(list(rp.values()), states)
