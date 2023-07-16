23253878ebcffec548b1709cdbd0e864ddddd3e0
luccaz <luccazoppi@gmail.com>
2023-07-15 22:42:18 -0300

    changed to slippery version

--- evaluate.py
+++ evaluate.py
@@ -1,28 +1,33 @@
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+from gymnasium.wrappers.time_limit import TimeLimit
 import numpy as np
 import matplotlib.pyplot as plt
 from dqn_agent import DQNAgent
-from utils import reward_engineering
+from utils import reward_engineering, BOARD_SZ, TIME_LIMIT, MAP_NAME, FIXED_SEED
 import tensorflow as tf
 
-def plot_points(point_list, style):
-    x = []
-    y = []
-    for point in point_list:
-        x.append(point[0])
-        y.append(point[1])
-    plt.plot(x, y, style)
+# def plot_points(point_list, style):
+#     x = []
+#     y = []
+#     for point in point_list:
+#         x.append(point[0])
+#         y.append(point[1])
+#     plt.plot(x, y, style)
 
 
-NUM_EPISODES = 30  # Number of episodes used for evaluation
+NUM_EPISODES = 20  # Number of episodes used for evaluation
 RENDER = True
 
 tf.compat.v1.disable_eager_execution()
 
-map_desc = generate_random_map(size=4, seed = 0)
-env = gym.make('FrozenLake-v1', desc=map_desc, map_name="4x4", is_slippery=True, render_mode='human' if RENDER else None)
+map_desc = generate_random_map(size=BOARD_SZ, seed = FIXED_SEED)
+env = gym.make('FrozenLake-v1', desc=map_desc, map_name=MAP_NAME, is_slippery=True, render_mode='human' if RENDER else None)
+env = TimeLimit(env, TIME_LIMIT*BOARD_SZ)
 action_size = env.action_space.n
 
 # Creating the DQN agent (with greedy policy, suited for evaluation)
@@ -37,36 +42,40 @@ else:
     exit(-1)
 return_history = []
 time_history = []
-for episodes in range(1, NUM_EPISODES + 1):
+rate_of_completion = 0.0
+for episode in range(1, NUM_EPISODES + 1):
     # Reset the environment
     state, _ = env.reset()
     state = np.array([state])
     # Cumulative reward is the return since the beginning of the episode
     cumulative_reward = 0.0
-    for time in range(1, 100):
+    prev_action = -1
+    for time in range(1, TIME_LIMIT + 1):
         # Render the environment for visualization
         if RENDER:
             env.render()
         # Select action
         action = agent.act(state)
         # Take action, observe reward and new state
-        next_state, reward, terminated, _, _ = env.step(action)
+        next_state, completed, terminated, truncated, _ = env.step(action)
         next_state = np.array([next_state])
         # Making reward engineering to keep compatibility with how training was done
-        reward = reward_engineering(state, action, reward, next_state)
+        reward = reward_engineering(state, prev_action, action, completed, next_state, terminated, truncated)
+        prev_action = action
         state = next_state
         # Accumulate reward
         cumulative_reward = agent.gamma * cumulative_reward + reward
-        if terminated:
-            print("episode: {}/{}, time: {}, score: {:.6}, epsilon: {:.3}"
-                  .format(episodes, NUM_EPISODES, time, cumulative_reward, agent.epsilon))
+        if terminated or truncated:
+            print("episode: {}/{}, time: {}, score: {:.6f}, epsilon: {:.3f}"
+                  .format(episode, NUM_EPISODES, time, cumulative_reward, agent.epsilon))
             time_history.append(time)
             break
     return_history.append(cumulative_reward)
-
+    rate_of_completion += (completed - rate_of_completion)/episode
 # Prints mean return
 print('Mean return: ', np.mean(return_history))
 print('Mean time: ', np.mean(time_history))
+print('Rate of completion: {:.2f}%'.format(rate_of_completion*100))
 
 
 # Plots return history
@@ -74,32 +83,3 @@ plt.plot(return_history, 'b')
 plt.xlabel('Episode')
 plt.ylabel('Return')
 plt.savefig('dqn_evaluation.eps', format='eps')
-
-# # Plots the greedy policy learned by DQN
-# plt.figure()
-# position = np.arange(-1.2, 0.5 + 0.025, 0.05)
-# velocity = np.arange(-0.07, 0.07 + 0.0025, 0.005)
-# push_left = []
-# none = []
-# push_right = []
-# for j in range(len(position)):
-#     for k in range(len(velocity)):
-#         pos = position[j]
-#         vel = velocity[k]
-#         state = np.array([[pos, vel]])
-#         action = agent.act(state)
-#         if action == 0:
-#             push_left.append(state[0])
-#         elif action == 1:
-#             none.append(state[0])
-#         else:
-#             push_right.append(state[0])
-# plot_points(push_left, 'b.')
-# plot_points(none, 'r.')
-# plot_points(push_right, 'g.')
-# plt.xlabel('Position')
-# plt.ylabel('Velocity')
-# plt.title('Agent Policy')
-# plt.legend(['Left', 'None', 'Right'])
-# plt.savefig('agent_decision.eps', format='eps')
-# plt.show()
--- train_agent.py
+++ train_agent.py
@@ -1,19 +1,23 @@
 import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+from gymnasium.wrappers.time_limit import TimeLimit
 import numpy as np
 import matplotlib.pyplot as plt
 from dqn_agent import DQNAgent
-from utils import reward_engineering
+from utils import reward_engineering, BOARD_SZ, TIME_LIMIT, MAP_NAME, FIXED_SEED
 import tensorflow as tf
 
 
 NUM_EPISODES = 300  # Number of episodes used for training
-
 tf.compat.v1.disable_eager_execution()
 
-map_desc = generate_random_map(size=4, seed = 0)
-env = gym.make('FrozenLake-v1', desc=map_desc, map_name="4x4", is_slippery=True)
+map_desc = generate_random_map(size=BOARD_SZ, seed = FIXED_SEED)
+env = gym.make('FrozenLake-v1', desc=map_desc, map_name=MAP_NAME, is_slippery=True)
+env = TimeLimit(env, TIME_LIMIT)
 action_size = env.action_space.n
 
 # Creating the DQN agent
@@ -26,46 +30,47 @@ if os.path.exists('frozen_lake.h5'):
 else:
     print('No weights found from previous learning session.')
 done = False
-batch_size = 4  # batch size used for the experience replay
+batch_size = BOARD_SZ  # batch size used for the experience replay
 return_history = []
 
-for episodes in range(1, NUM_EPISODES + 1):
-    print(episodes)
+for episode in range(1, NUM_EPISODES + 1):
+    #print(episodes)
     # Reset the environment
     state, _ = env.reset()
     state = np.array([state])
     # Cumulative reward is the return since the beginning of the episode
     cumulative_reward = 0.0
-    for time in range(1, 100):
+    prev_action = -1
+    for time in range(1, TIME_LIMIT+1):
         # Select action
         action = agent.act(state)
         # Take action, observe reward and new state
-        next_state, reward, terminated, _, _ = env.step(action)
+        next_state, completed, terminated, truncated, _ = env.step(action)
         next_state = np.array([next_state])
         # Making reward engineering to allow faster training
-        reward = reward_engineering(state, action, reward, next_state)
+        reward = reward_engineering(state, prev_action, action, completed, next_state, terminated, truncated)
+        prev_action = action
         # Appending this experience to the experience replay buffer
-        agent.append_experience(state, action, reward, next_state, terminated)
+        agent.append_experience(state, action, reward, next_state, completed)
         state = next_state
         # Accumulate reward
         cumulative_reward = agent.gamma * cumulative_reward + reward
-        if terminated:
+        if terminated or truncated:
             print("episode: {}/{}, time: {}, score: {:.6}, epsilon: {:.3}"
-                  .format(episodes, NUM_EPISODES, time, cumulative_reward, agent.epsilon))
+                  .format(episode, NUM_EPISODES, time, cumulative_reward, agent.epsilon))
             break
         # We only update the policy if we already have enough experience in memory
         if len(agent.replay_buffer) > 2 * batch_size:
             loss = agent.replay(batch_size)
     return_history.append(cumulative_reward)
     agent.update_epsilon()
-    # Every 10 episodes, update the plot for training monitoring
-    if episodes % 20 == 0:
-        plt.plot(return_history, 'b')
-        plt.xlabel('Episode')
-        plt.ylabel('Return')
-        plt.show(block=False)
-        plt.pause(0.1)
-        plt.savefig('dqn_training.eps', format='eps')
-        # Saving the model to disk
+    if episode % 20:
+        #Saving the model to disk
         agent.save("frozen_lake.h5")
-plt.pause(1.0)
+#         plt.plot(return_history, 'b')
+#         plt.xlabel('Episode')
+#         plt.ylabel('Return')
+#         plt.show(block=False)
+#         plt.pause(0.1)
+#         plt.savefig('dqn_training.eps', format='eps')
+# plt.pause(1.0)
