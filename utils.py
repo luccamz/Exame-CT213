0758c5f8b05d26c9c6d99025f131aebd6279b373
luccaz <luccazoppi@gmail.com>
2023-07-17 23:34:23 -0300

    increased time limit

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 from numpy import dot
 
 # gym enviroment params
-BOARD_SZ = 7
+BOARD_SZ = 4
 MAP_NAME = "{}x{}".format(BOARD_SZ, BOARD_SZ)
-TIME_LIMIT = 20*BOARD_SZ
+TIME_LIMIT = 25*BOARD_SZ
 FIXED_SEED = 0
-SLIPPERY = False
+SLIPPERY = True
 
 # maps the flattened state displacement to the corresponding action
 displacement_to_action = { 
-     0 : -1,
+     0 : 6,
     -BOARD_SZ : 3,
     -1 : 0,
      1 : 2,
@@ -19,12 +19,12 @@ displacement_to_action = {
 # rewards and punishments coefficients
 rp = {
     "completed" : 100.,
-    "goal_direction" : 2.,
-    #"deliberate_action" : 10.,
+    "goal_direction" : 1.,
+    #"deliberate_action" : 2.,
     #"lost_on_time" : -80.,
     "stuck" : -3.,
-    #"moving_backwards" : -100.,
-    "fell_in_hole" : -100.,
+    #"moving_backwards" : -10.,
+    "fell_in_hole" : -1000.,
     #"at_corner" : -100
 }
 
@@ -52,7 +52,7 @@ def reward_engineering(state: int, prev_action: int, action: int, completed: int
     displacement = next_state - state
     action_taken = displacement_to_action[displacement]
     deliberate_action = int(action_taken == action)
-    goal_direction = 1 if (action % 3) != 0 else -1
+    goal_direction = 2 + 0.5*(action_taken % 3 != 0) if (action % 3) != 0 else -1
     stuck = int(displacement == 0)
     fell_in_hole = int(completed == 0 and terminated and not lost_on_time)
     moving_backwards = (1 + prev_action - action) % 2
