bc2c3ed6fd5255908067ffe237255914b09871ef
luccaz <luccazoppi@gmail.com>
2023-07-20 02:46:41 -0300

    changed num of episodes and plots params

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,6 @@
 import os
 import gymnasium as gym
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
-from gymnasium.wrappers.time_limit import TimeLimit
 import numpy as np
 import matplotlib.pyplot as plt
 from dqn_agent import DQNAgent
@@ -9,12 +8,13 @@ from utils import reward_engineering, BOARD_SZ, TIME_LIMIT, MAP_NAME, FIXED_SEED
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-NUM_EPISODES = 50  # Number of episodes used for evaluation
+NUM_EPISODES = 1000  # Number of episodes used for evaluation
 RENDER = False # choose whether to show the GUI of the gym environment
+if RENDER:
+    NUM_EPISODES = 5
 
 map_desc = generate_random_map(size=BOARD_SZ, seed = FIXED_SEED)
-env = gym.make('FrozenLake-v1', desc=map_desc, map_name=MAP_NAME, is_slippery=SLIPPERY, render_mode='human' if RENDER else 'rgb_array')
-env = TimeLimit(env, TIME_LIMIT)
+env = gym.make('FrozenLake-v1', desc=map_desc, map_name=MAP_NAME, is_slippery=SLIPPERY, render_mode='human' if RENDER else 'rgb_array', max_episode_steps=TIME_LIMIT)
 action_size = env.action_space.n
 observation_sz = env.observation_space.n
 
@@ -23,13 +23,13 @@ agent = DQNAgent(observation_sz, action_size, epsilon=0.0, epsilon_min=0.0)
 
 # Checking if weights from previous learning session exists
 if os.path.exists('frozen_lake.pkl'):
-    print('Loading weights from previous learning session.')
+    print('Loading Q table from previous learning session.')
     agent.load("frozen_lake.pkl")
 else:
-    print('No weights found from previous learning session. Unable to proceed.')
+    print('No Q table found from previous learning session. Unable to proceed.')
     exit(-1)
 
-# print(agent.q) # for troubleshooting
+#print(agent.q) # for troubleshooting
 
 slip = '' if SLIPPERY else '_not_slippery'
 values = agent.q_max_vals()
@@ -43,13 +43,14 @@ sns.heatmap(
         linecolor="black",
         xticklabels=[],
         yticklabels=[],
-        annot_kws={"fontsize": "xx-large"},
-    ).set(title="Learned Q-values\nArrows represent best action")
-plt.savefig("learned_greedy_"+MAP_NAME+"_seed{}".format(FIXED_SEED)+slip+".eps")
+        annot_kws={"fontsize": "xx-large"}, # change fontsize for greater board sizes 
+    )
+plt.savefig("learned_greedy_"+MAP_NAME+"_seed{}".format(FIXED_SEED)+slip+".eps", bbox_inches='tight')
 
 return_history = []
 time_history = []
 rate_of_completion = 0.0
+rate_of_loss_on_time = 0.0
 for episode in range(1, NUM_EPISODES + 1):
     # Reset the environment
     state, _ = env.reset()
@@ -66,18 +67,20 @@ for episode in range(1, NUM_EPISODES + 1):
         # Take action, observe reward and new state
         next_state, completed, terminated, truncated, _ = env.step(action)
         # Making reward engineering to keep compatibility with how training was done
-        reward = reward_engineering(state, prev_action, action, completed, next_state, terminated, truncated)
+        reward = reward_engineering(state, prev_action, action, completed, next_state, terminated, truncated and not completed)
         prev_action = action
         state = next_state
         # Accumulate reward
         cumulative_reward += f*reward
         f *= agent.gamma
         if terminated or truncated:
-            print("episode: {}/{}, time: {}, score: {:.6f}"
-                  .format(episode, NUM_EPISODES, time, cumulative_reward))
+            if episode % 10 == 0:
+                print("episode: {}/{}, time: {}, score: {:.6f}"
+                      .format(episode, NUM_EPISODES, time, cumulative_reward))
             time_history.append(time)
             break
     return_history.append(cumulative_reward)
+    rate_of_loss_on_time += ((truncated and not completed)- rate_of_loss_on_time)/episode
     rate_of_completion += (completed - rate_of_completion)/episode
 
 if not RENDER:
@@ -87,10 +90,11 @@ if not RENDER:
 print('Mean return: ', np.mean(return_history))
 print('Mean time: ', np.mean(time_history))
 print('Rate of completion: {:.2f}%'.format(rate_of_completion*100))
+print('Rate of loss on time: {:.2f}%'.format(rate_of_loss_on_time*100))
 
 # Plots return history
 plt.figure()
 plt.plot(return_history, 'b')
 plt.xlabel('Episode')
 plt.ylabel('Return')
-plt.savefig("dqn_evaluation_"+MAP_NAME+"_seed{}".format(FIXED_SEED)+slip+".eps", format='eps')
+plt.savefig("dqn_evaluation_"+MAP_NAME+"_seed{}".format(FIXED_SEED)+slip+".eps", format='eps', bbox_inches='tight')
