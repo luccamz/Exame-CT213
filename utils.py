e513107b98f1a43e69a72df146c694b32797372f
luccaz <luccazoppi@gmail.com>
2023-07-20 02:47:37 -0300

    changed rewards and punishments

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
 from numpy import dot
 
 # gym enviroment params
-BOARD_SZ = 6 # side length for the frozen lake env board
+BOARD_SZ = 4 # side length for the frozen lake env board
 BOARD_SHAPE = (BOARD_SZ, BOARD_SZ) 
 MAP_NAME = "{}x{}".format(BOARD_SZ, BOARD_SZ)
 TIME_LIMIT = 25*BOARD_SZ # number of time steps before truncation
 FIXED_SEED = 0 # seed for random map generation
-SLIPPERY = False # whether ground is slippery or not
+SLIPPERY = True # whether ground is slippery or not
 
 # maps the flattened state displacement to the corresponding action
 displacement_to_action = { 
@@ -19,14 +19,14 @@ displacement_to_action = {
 
 # rewards and punishments coefficients
 rp = {
-    "completed" : 100.,
+    "completed" : 1000.,
     "goal_direction" : 1.,
-    #"deliberate_action" : 2.,
-    #"lost_on_time" : -80.,
-    "stuck" : -2.,
-    #"moving_backwards" : -10.,
-    "fell_in_hole" : -1000.,
-    #"at_corner" : -100
+    "deliberate_action" : 1.,
+    #"lost_on_time" : 10.,
+    "stuck" : -2.5,
+    "moving_backwards" : -1.,
+    "fell_in_hole" : -700.,
+    "towards_hole" : -700
 }
 
 def reward_engineering(state: int, prev_action: int, action: int, completed: int, next_state: int, terminated: bool, lost_on_time: bool) -> float:
@@ -53,11 +53,11 @@ def reward_engineering(state: int, prev_action: int, action: int, completed: int
     displacement = next_state - state
     action_taken = displacement_to_action[displacement]
     deliberate_action = int(action_taken == action)
-    goal_direction = 2 + 0.5*(action_taken % 3 != 0) if (action % 3) != 0 else -1
+    goal_direction = 2 + 0.5*(action_taken % 3 != 0) if (action % 3) != 0 else -5
     stuck = int(displacement == 0)
     fell_in_hole = int(completed == 0 and terminated and not lost_on_time)
+    towards_hole = int(fell_in_hole and deliberate_action)
     moving_backwards = (1 + prev_action - action) % 2
-    at_corner = 1 if state == 12 or state == 3 else 0
     loc = locals()
     states = list(map(loc.get, rp.keys()))
     return dot(list(rp.values()), states) 
