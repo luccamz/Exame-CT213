1dd47e89bc6d9f15cbfc874bd098a0e4c455c5df
luccaz <luccazoppi@gmail.com>
2023-07-20 19:32:41 -0300

    improved comments

--- utils.py
+++ utils.py
@@ -24,7 +24,7 @@ displacement_to_action = {
 
 #if True, then loads weigths for general purpose, else, loads optimized weigths for particular problem
 # that being the 6 x 6 slippery with seed 0
-GENERAL_RP = True # TODO ! User, don't change this to False
+GENERAL_RP = False # TODO ! User, don't change this to False
 
 # rewards and punishments coefficients
 path = 'general.json' if GENERAL_RP else 'particular.json'
@@ -56,11 +56,9 @@ def reward_engineering(state: int, prev_action: int, action: int, completed: int
 
     # the way this works is, you add an entry to the json file containing the weight, then
     # in the body of this function you define a variable with the exact same name, that should evaluate
-    # to 0 if the weight shouldn't be applied and to 1 if it should (so, essentially a boolean). 
+    # to 0 if the weight shouldn't be applied and to 1 if it should (so, essentially, a boolean). 
     # There's no need to consider signs in here because those go in the weights. If you want to exclude any
-    # weight then you can either delete its entry from the json from which it's being read or delete the local 
-    # variable here (or set either one to 0, obviously), therefore, it's a better practice to control it 
-    # thorugh the json file, so you don't need to change the function each time
+    # weight then you can delete its entry from the json from which it's being read or set it to 0.
      
     displacement = next_state - state
     action_taken = displacement_to_action[displacement]
@@ -68,6 +66,7 @@ def reward_engineering(state: int, prev_action: int, action: int, completed: int
     goal_direction = (action % 3 != 0)
     went_goal_direction =  (action_taken % 3 != 0)
     opposite_direction = (action % 3 == 0)
+    went_opposite_direction = (action_taken % 3 == 0)
     stuck = int(displacement == 0)
     fell_in_hole = int(completed == 0 and terminated and not lost_on_time)
     towards_hole = int(fell_in_hole and deliberate_action)
